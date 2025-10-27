545a7c7b41061c1fff68eea05ac5cbd2664d8753
James0507-cell <bunbee143@gmail.com>
2025-10-27 20:32:04 +0800

    dad

---
+++ WpfApp2/dbManager.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class dbManager
+    {
+        private MySqlConnection dbConn;
+        private MySqlCommand dbCommand;
+        private MySqlDataAdapter da;
+        private DataTable dt;
+        private string strConn = "server=localhost;user id=root;password=;database=db_medicaremmcm";
+
+
+        public void dbConnection()
+        {
+            dbConn = new MySqlConnection(strConn);
+            dbConn.Open();
+            dbConn.Close();
+        }
+
+        public DataTable displayRecords(string query)
+        {
+            dbConn = new MySqlConnection(strConn);
+            dbConn.Open();
+            da = new MySqlDataAdapter(query, dbConn);
+            dt = new DataTable();
+            da.Fill(dt);
+            dbConn.Close();
+            return dt;
+        }
+
+        public void sqlManager(string query)
+        {
+            dbConn = new MySqlConnection(strConn);
+            dbConn.Open();
+            dbCommand = new MySqlCommand(query, dbConn);
+            dbCommand.ExecuteNonQuery();
+            dbConn.Close();
+        }
+    }
+}
